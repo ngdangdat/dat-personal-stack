c12cf1db04b18e1b9c5f46e589ddcb3bb2de678f
ngdangdat <ngdangdat09@gmail.com>
2026-05-21 00:20:29 +0900

    chore: run gofmt on backend source files

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -25,7 +25,7 @@ func main() {
 			w.Header().Set("Access-Control-Allow-Origin", "*")
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-			
+
 			if r.Method == "OPTIONS" {
 				w.WriteHeader(http.StatusOK)
 				return
